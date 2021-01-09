1054aa3fb97bd74f63010fb7ea2f2df8f8cdc60a
Peter Grace <pete.grace@gmail.com>
2021-01-09 23:49:40 +0000

    update benchmark script

--- benchmark.sh
+++ benchmark.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 PATTERNS=${PATTERNS:-$(ls -1 patterns/ | tr '\n' ' ')}
+LOGDIR=/tmp/recrossable
+mkdir -p $LOGDIR
 for i in $PATTERNS
 do
-    echo "Benchmarking: $i"
-    timeout 10 ssh root@remarkable RECROSSABLE_BENCHMARKING=1 RECROSSABLE_PUZZLENAME=$i /opt/bin/recrossable 2>&1 | grep Generated || echo "timeout"
-    echo "----"
+    if [[ ! -f "${LOGDIR}/${i}.tested" ]];
+    then
+        echo "Benchmarking: $i"
+        timeout 30 ssh root@remarkable RECROSSABLE_BENCHMARKING=1 RECROSSABLE_PUZZLENAME=$i /opt/bin/recrossable 2>&1 | grep Generated || echo "timeout"
+        touch ${LOGDIR}/${i}.tested
+        echo "----"
+    fi
 done
