f732e9e03d308e93b6a86977344aa1338bc90e98
Peter Grace <pete.grace@gmail.com>
2021-01-09 23:42:35 +0000

    disable more words that take too long

--- benchmark.sh
+++ benchmark.sh
@@ -3,8 +3,6 @@ PATTERNS=${PATTERNS:-$(ls -1 patterns/ | tr '\n' ' ')}
 for i in $PATTERNS
 do
     echo "Benchmarking: $i"
-    ssh root@remarkable RECROSSABLE_BENCHMARKING=1 RECROSSABLE_PUZZLENAME=$i /opt/bin/recrossable 2>&1 | grep Generated
-    ssh root@remarkable RECROSSABLE_BENCHMARKING=1 RECROSSABLE_PUZZLENAME=$i /opt/bin/recrossable 2>&1 | grep Generated
-    ssh root@remarkable RECROSSABLE_BENCHMARKING=1 RECROSSABLE_PUZZLENAME=$i /opt/bin/recrossable 2>&1 | grep Generated
+    timeout 10 ssh root@remarkable RECROSSABLE_BENCHMARKING=1 RECROSSABLE_PUZZLENAME=$i /opt/bin/recrossable 2>&1 | grep Generated || echo "timeout"
     echo "----"
 done
