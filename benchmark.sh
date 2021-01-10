d26c3b9e285896f12c031db0be468bcea6cfa09e
Peter Grace <pete.grace@gmail.com>
2021-01-10 00:44:21 +0000

    further benchmark script fixes

--- benchmark.sh
+++ benchmark.sh
@@ -7,8 +7,13 @@ do
     if [[ ! -f "${LOGDIR}/${i}.tested" ]];
     then
         echo "Benchmarking: $i"
-        timeout 30 ssh root@remarkable RECROSSABLE_BENCHMARKING=1 RECROSSABLE_PUZZLENAME=$i /opt/bin/recrossable 2>&1 | grep Generated || echo "timeout"
-        touch ${LOGDIR}/${i}.tested
+        timeout 30 ssh root@remarkable RECROSSABLE_BENCHMARKING=1 RECROSSABLE_PUZZLENAME=$i /opt/bin/recrossable 2>&1 | grep Generated
+        if [[ $? -ne 0 ]];
+        then
+            echo "timeout"
+        else
+            touch ${LOGDIR}/${i}.tested
+        fi
         echo "----"
     fi
 done
